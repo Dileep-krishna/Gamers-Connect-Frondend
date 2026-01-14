efe86a9ff991958eb7c0beaf29bb95b2fc63ff98
Dileep-krishna <dileepkrishna7178@gmail.com>
2026-01-14 23:28:06 +0530

    completed

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,4 +1,5 @@
-const SERVERURL="http://localhost:3000"
+// const SERVERURL="http://localhost:3000"
 // const SERVERURL="https://gamers-connect-backend.onrender.com"
+const SERVERURL="https://gamers-connect-backend-3.onrender.com"
 
 export default SERVERURL
